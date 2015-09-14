0db7a161844f271d5ccdcf5c83bb2367576e8ad2
Yassine Khachlek <yassine.khachlek@gmail.com>
2015-09-14 08:20:31 +0200

    added leveldb as local storage engine

---
+++ database/sample_user.js
@@ -0,0 +1,42 @@
+var levelup = require('level')
+
+var db = levelup('./database/user')
+
+var users = [];
+
+for(i=1;i<10;+i++){
+  users.push({
+    "id": i,
+    "username": "username_"+i,
+    "password": "password_"+i,
+  });
+}
+
+// Insert users sample data
+
+users.forEach(function(user, index){
+
+  db.get(user.id, function (err, userValue) {
+
+    if (err) { // the key was not found
+
+      console.log("NOT FOUND: ", err);
+
+      db.put(user.id, JSON.stringify(user), function (err) {
+        
+        if (err){ // some kind of I/O error
+          console.log(err);
+        }else{
+            console.log("ADDED", JSON.stringify(user));
+        }
+
+      })
+
+    }else{
+      console.log("EXIST", userValue);
+    }
+
+  })
+
+});
+
